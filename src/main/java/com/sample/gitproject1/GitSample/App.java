925bf48fddfdf87a082d07b714175181bc89bdc2
Architha-15 <architha.gadi93@gmail.com>
2020-05-10 18:23:54 +0530

    Added all files

---
+++ src/main/java/com/sample/gitproject1/GitSample/App.java
@@ -0,0 +1,13 @@
+package com.sample.gitproject1.GitSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
