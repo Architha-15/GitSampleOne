53d5e5d138e17354d26337ee136f386cd4b3f68a
Architha-15 <architha.gadi93@gmail.com>
2020-05-10 23:33:29 +0530

    updated by person2

--- src/test/java/com/sample/gitproject1/GitSample/GitTest.java
+++ src/test/java/com/sample/gitproject1/GitSample/GitTest.java
@@ -3,5 +3,6 @@ package com.sample.gitproject1.GitSample;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("person2");
+		System.out.println("person2 code......");
 	}
 }
